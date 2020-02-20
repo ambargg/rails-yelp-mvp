45d419aae8f3a1f72cd85481835273d19ca24521
Ambar Galan <ambar.galan@gmail.com>
2020-02-20 08:57:53 +0100

    Making two models in Rail to give reviews to Restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  '01 43 54 23 31',
+    category:      'japanese'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  '01 43 54 23 31',
+    category:      'italian'
+  },
+  {
+    name:         "La Tour d'Argent",
+    address:      '15 Quai de la Tournelle, 75005 Paris',
+    phone_number:  '01 43 54 23 31',
+    category:      'french'
+  },
+  {
+    name:         "Franco Manca",
+    address:      '4 Market Row, Brixton, London SW9 8LD',
+    phone_number:  '+44 20 7738 3021',
+    category:      'italian'
+  },
+  {
+    name:         "Eat Tokyo",
+    address:      '16 Old Compton St, Soho, London W1D 4TL',
+    phone_number:  '+44 20 7439 9887',
+    category:      'japanese'
+  }
+]
+
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
